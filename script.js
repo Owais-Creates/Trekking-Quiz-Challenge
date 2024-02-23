e4b6e06221c8cb90c3aa4887cee6219804d76e40
Owais <zaidalit00@gmail.com>
2024-02-23 22:11:53 +0530

    added questions

---
+++ script.js
@@ -0,0 +1,68 @@
+// Questions
+
+const questions = [
+    {
+        question: "What is the highest mountain in the world?",
+        option: [
+            'K2',
+            'Mount Everest',
+            'Kanchenjunga',
+            'Lhotse'
+        ],
+        answer: 1
+    },
+
+    {
+
+        question: "What is the term for a professional guide who leads trekkers through difficult terrains and landscapes?",
+        option: [
+            'Ranger',
+            'Navigator',
+            'Sherpa',
+            'Pathfinder'
+        ],
+        answer: 3
+
+    },
+
+    {
+
+        question: 'Which type of footwear is commonly recommended for trekking over rugged terrain',
+        option: [
+            'Hiking boots',
+            'Climbing shoes',
+            'Trail running shoes',
+            'Moccasins'
+        ],
+        answer: 0
+
+    },
+
+    {
+
+        question: 'What is the term for a shelter built to provide temporary protection from the weather during a trek?',
+        option: [
+            'Cabin',
+            'Lodge',
+            'Tent',
+            'Bivouac'
+        ],
+        answer: 4
+
+    },
+
+    {
+
+        question: 'What is the term for the process of getting used to high altitudes before undertaking a trek?',
+        option: [
+            'Altitude Training',
+            'Base camp preparation',
+            'Acclimatization',
+            'Climbing condition',
+        ],
+        answer: 3
+
+    }
+
+]
+
